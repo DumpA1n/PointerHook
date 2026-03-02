38810ad87b4abd102fc87742e3c085394d21d9e7
DumpA1n <dengjiang0331@gmail.com>
2026-03-02 14:50:25 +0800

    chore: update CMakeLists

--- example.cpp
+++ example.cpp
@@ -9,27 +9,6 @@
 
 static ElfScanner g_UE4;
 
-class AimAssistor : public SafePointerHook
-{
-public:
-    AimAssistor() : SafePointerHook() {}
-    ~AimAssistor() override = default;
-
-    std::string GetName() const override { return "AimAssistor"; }
-
-    uintptr_t FakeFunction(RegContext *ctx) override
-    {
-        LOGI("[%s] s0: %f", GetName().c_str(), ctx->floating.s[0]);
-        ctx->floating.s[0] = 1.25f;
-        return GetOrigFuncAddr();
-    }
-
-protected:
-    uintptr_t GetElfBaseImpl() const override { return g_UE4.base(); }
-    uintptr_t GetPtrAddrImpl() const override { return GetElfBaseImpl() + 0x14053D30; }
-    uintptr_t GetFuncAddrImpl() const override { return GetElfBaseImpl() + 0x8E97190; }
-};
-
 class MemcpyHook : public SafePointerHook
 {
 public:
@@ -59,7 +38,6 @@ void main_thread()
 
     LOGI("[+] libUE4.so base: %p", g_UE4.base());
 
-    PointerHookManager::GetInstance().Add<AimAssistor>();
     PointerHookManager::GetInstance().Add<MemcpyHook>();
 }
 
